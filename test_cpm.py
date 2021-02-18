c0f53c3fd556c0510a7801c1528a3de881c7bdbe
mpampolina <miguel.r.pampolina@gmail.com>
2021-02-18 12:32:14 -0800

    add additional test scenario

--- test_cpm.py
+++ test_cpm.py
@@ -1,19 +1,24 @@
 import unittest
-from test_utilities import *
-import pprint
 from cpm import Cpm
-
-pp = pprint.PrettyPrinter(indent=4)
-
+from test_utilities import *
 
 class TestCalc(unittest.TestCase):
     def setUp(self):
+        self.cpm1 = Cpm(testActivities1)
+        self.cpm1.run()
         self.cpm2 = Cpm(testActivities2)
         self.cpm2.run()
+        self.cpm3 = Cpm(testActivities3)
+        self.cpm3.run()
 
-    def test_add(self):
+    # Check for a discrepancy between the 
+    # given reference list items (including nested dictionaries
+    # or lists) and the the class generated  list items, irrespective
+    # of order
+    def test_equalData(self):
+        self.assertCountEqual(self.cpm1.activities, testResults1)
         self.assertCountEqual(self.cpm2.activities, testResults2)
-
+        self.assertCountEqual(self.cpm3.activities, testResults3)
 
 if __name__ == "__main__":
     unittest.main()
